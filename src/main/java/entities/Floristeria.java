f96f87c9a6b75fc75f4907a7ae376e774beccbab
rokysf <135695631+rokysf@users.noreply.github.com>
2023-09-21 11:54:38 +0200

    Add files via upload

--- src/main/java/entities/Floristeria.java
+++ src/main/java/entities/Floristeria.java
@@ -3,5 +3,36 @@ package entities;
 public class Floristeria {
 	private Long id;
 	private String nom;
+	
+	public Floristeria(Long id, String nom) {
+		this.id = id;
+		this.nom = nom;
+	}
+	
+	public Floristeria() {
+		
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Floristeria [id=" + id + ", nom=" + nom + "]";
+	}
+
+	
 }
--- src/main/java/entities/Ticket.java
+++ src/main/java/entities/Ticket.java
@@ -2,4 +2,22 @@ package entities;
 
 public class Ticket {
 	private Long id;
+
+	public Ticket() {
+		
+	}
+	
+	public Ticket(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
 }
