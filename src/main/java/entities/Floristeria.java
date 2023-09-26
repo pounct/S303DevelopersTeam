da13d0cd9f1c6665163b4818de9599460c0cf935
user <email>
2023-09-26 12:41:21 +0200

    testOfPullJordi

--- src/main/java/entities/Floristeria.java
+++ src/main/java/entities/Floristeria.java
@@ -42,9 +42,28 @@ public class Floristeria {
 		return nom;
 	}
 
+<<<<<<< HEAD
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+=======
+    public List<Arbre> getArbres() {
+        return arbres;
+    }
+
+    public List<Flor> getFlors() {
+        return flors;
+    }
+
+    public List<Decoracio> getDecoracios() {
+        return decoracios;
+    }
+
+    @Override
+    public String toString() {
+        return "Floristeria [id=" + id + ", nom=" + nom + "]";
+    }
+>>>>>>> e49d224a6f65e8b95961eb8ca0dd609d9a8da54c
 
 	@Override
 	public String toString() {
--- src/main/java/entities/IGestion.java
+++ src/main/java/entities/IGestion.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface IGestion {
 
+<<<<<<< HEAD
 	Floristeria crearFloristeria(Long id, String nom);
 	void afegirArbre();
 	void afegirFlor();
@@ -11,6 +12,14 @@ public interface IGestion {
 	void retirarArbre(Arbre arbre, Floristeria floristeria);
 	void retirarFlor(Flor flor, Floristeria floristeria);
 	void retirarDecoracio(Decoracio decoracio, Floristeria floristeria);
+=======
+	void afegirArbre(Arbre arbol, Floristeria floristeria);
+	void afegirFlor(Flor flor, Floristeria floristeria);
+	void afegirDecoracio(Decoracio decoracio, Floristeria floristeria);
+	void retirarArbre();
+	void retirarFlor();
+	void retirarDecoracio();
+>>>>>>> e49d224a6f65e8b95961eb8ca0dd609d9a8da54c
 	void imprimirStock();
 	void imprimirStockQuantitats();
 	void imprimirStockValor();
